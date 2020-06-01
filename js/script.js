1821f1e8de8e285800480a89a9e285e20f23b5ec
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-06-01 21:00:58 +0200

    Add creating a new array with map method

--- js/script.js
+++ js/script.js
@@ -3,11 +3,16 @@ const passwords = ['jedEN', 'DwA'];
 const messages = ['super', 'it works!']
 const div = document.querySelector('div');
 
+//1. changing the array
 passwords.forEach((password, index) => {
   passwords[index] = password.toLowerCase();
   //console.log(passwords[index]);
  })
  
+ //2. creating a new array
+ const LCPasswords = passwords.map(password => password.toLowerCase())
+
+//3. changing the letter size inside the function only
 
 // const showMessage = (e) => {
 //   const text = e.target.value;
@@ -20,15 +25,28 @@ passwords.forEach((password, index) => {
 //   })
 // }
 
-const showMessage = (e) => {
-  const text = e.target.value.toLowerCase();
+//1. changing the array
+
+// const showMessage = (e) => {
+//   const text = e.target.value.toLowerCase();
  
-  passwords.forEach((password, index) => {
-    if(password === text) {
-      div.textContent = messages[index];
-      e.target.value = '';
-    } 
-  })
-}
+//   passwords.forEach((password, index) => {
+//     if(password === text) {
+//       div.textContent = messages[index];
+//       e.target.value = '';
+//     } 
+//   })
+// }
+
+//2. creating a new array
+const showMessage = (e) => {
+  const textInput = e.target.value.toLowerCase();
+ 
+  for (let i = 0; i < LCPasswords.length; i++) {
+   if (textInput === LCPasswords[i]) {
+    div.innerHTML = messages[i];
+   }
+  }
+ }
 
 input.addEventListener('input', showMessage)
