7355784c92b92141046953d4f5069f16c6535d4b
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-06-01 20:38:16 +0200

    Add changing the letter size for the whole array with the forEach method

--- js/script.js
+++ js/script.js
@@ -3,13 +3,28 @@ const passwords = ['jedEN', 'DwA'];
 const messages = ['super', 'it works!']
 const div = document.querySelector('div');
 
+passwords.forEach((password, index) => {
+  passwords[index] = password.toLowerCase();
+  //console.log(passwords[index]);
+ })
+ 
+
+// const showMessage = (e) => {
+//   const text = e.target.value;
+ 
+//   passwords.forEach((password, index) => {
+//     if(password.toUpperCase() === text.toUpperCase()) {
+//       div.textContent = messages[index];
+//       e.target.value = '';
+//     } 
+//   })
+// }
 
 const showMessage = (e) => {
-  const text = e.target.value;
-  const newText = text.toUpperCase();
-  
+  const text = e.target.value.toLowerCase();
+ 
   passwords.forEach((password, index) => {
-    if(password.toUpperCase() === newText) {
+    if(password === text) {
       div.textContent = messages[index];
       e.target.value = '';
     } 
